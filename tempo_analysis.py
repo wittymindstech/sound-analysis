ddc2db1c7232799261990e900c769fbafefa0084
gauravshremayee <gaurav.2897@gmail.com>
2021-02-23 00:27:32 +0530

    updating variable name

--- tempo_analysis.py
+++ tempo_analysis.py
@@ -3,8 +3,8 @@
 import sys
 from aubio import tempo, source
 
-win_s = 512                 # fft size
-hop_s = win_s // 2          # hop size
+win_size = 512                 # fft size
+hop_size = win_size // 2          # hop size
 
 if len(sys.argv) < 2:
     print("Usage: %s <filename> [samplerate]" % sys.argv[0])
@@ -15,13 +15,13 @@ filename = sys.argv[1]
 samplerate = 0
 if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
 
-s = source(filename, samplerate, hop_s)
+s = source(filename, samplerate, hop_size)
 samplerate = s.samplerate
-o = tempo("default", win_s, hop_s, samplerate)
+o = tempo("default", win_size, hop_size, samplerate)
 
 # tempo detection delay, in samples
 # default to 4 blocks delay to catch up with
-delay = 4. * hop_s
+delay = 4. * hop_size
 
 # list of beats, in samples
 beats = []
@@ -32,9 +32,9 @@ while True:
     samples, read = s()
     is_beat = o(samples)
     if is_beat:
-        this_beat = int(total_frames - delay + is_beat[0] * hop_s)
+        this_beat = int(total_frames - delay + is_beat[0] * hop_size)
         print("%f" % (this_beat / float(samplerate)))
         beats.append(this_beat)
     total_frames += read
-    if read < hop_s: break
+    if read < hop_size: break
 #print len(beats)
